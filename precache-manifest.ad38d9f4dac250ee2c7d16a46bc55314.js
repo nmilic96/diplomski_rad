e2aa2eb1925788de62165006dc9c16c887e5a998
Nikola Milić <milic.n96@gmail.com>
2020-08-21 18:32:13 +0200

    Updates

---
+++ precache-manifest.ad38d9f4dac250ee2c7d16a46bc55314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc93e1f0464f6445154b1dea28d99ed",
+    "url": "/diplomski_rad/index.html"
+  },
+  {
+    "revision": "8d1223c7690a7bdce9c8",
+    "url": "/diplomski_rad/static/css/main.73a0f43c.chunk.css"
+  },
+  {
+    "revision": "31d20e2568d05f06ad38",
+    "url": "/diplomski_rad/static/js/2.82cba979.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diplomski_rad/static/js/2.82cba979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1223c7690a7bdce9c8",
+    "url": "/diplomski_rad/static/js/main.f11bb502.chunk.js"
+  },
+  {
+    "revision": "fe3a7e5d0a05c36aaca9",
+    "url": "/diplomski_rad/static/js/runtime-main.f2f3a613.js"
+  }
+]);
